21e242d2a0ec28b6492c7a86650a879679eeaf98
liuyi103 <liuyicheng1991@hotmail.com>
2014-11-15 22:28:25 +0800

    two codes

---
+++ datagen.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 15 21:31:10 2014
+
+@author: liuyi103
+"""
+
+import sys
+import numpy as np
+n=1000
+if len(sys.argv)>1:
+    n=int(sys.argv[0])
+pos=np.random.randint(0,100,(n,2))
+p=np.random.uniform(0.15,0.25,n)
+q=np.random.random(n)
+sell=np.random.randint(0,2,n)
+m=0
+f=file('data.txt','w')
+f.write('n=%d\n'%n)
+for i in range(n):
+    f.write('g.add_node(%d)\n'%i)
+    f.write('sell[%d],p[%d],q[%d],pos[%d]=%d,%lf,%lf,[%lf,%lf]\n'%(i,i,i,i,sell[i],p[i],q[i],pos[i][0],pos[i][1]))
+for i in range(n):
+    for j in range(n):
+        if sell[i]==1 and sell[j]==0 and np.sum((pos[i]-pos[j])**2)<10 and p[i]<p[j]:
+            m+=1
+            f.write('g.add_edge(%d,%d)\n'%(i,j))
+            f.write('th[%d,%d]=th[%d,%d]=min(q[%d]/3,q[%d]/3)\n'%(i,j,j,i,i,j))
+f.write('m=%d\n'%m)
+f.close()
+            
+    
---
+++ greedy.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  4 13:26:43 2014
+
+@author: liuyi103
+"""
+
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import pulp as pp
+import sys
+
+n=100
+if len(sys.argv)>1:
+    n=int(sys.argv[0])
+g=nx.DiGraph()
+pos={}
+sell={}
+sw=0
+q={}
+p={}
+th={}
+#for i in range(n):
+#    g.add_node(i)
+#    sell[i]=np.random.randint(0,2)
+#    pos[i]=np.random.rand(2)
+#    q[i]=np.random.rand(1)*5+1
+#    p[i]=np.random.rand(1)
+#for i in range(100):
+#    for j in range(100):
+#        if sell[i]==1 and sell[j]==0 and np.sum((pos[i]-pos[j])**2)<0.1 and p[i]<p[j]:
+#            g.add_edge(i,j)
+f=file('data.txt','r')
+for i in f:
+    exec i
+print m
+f.close()
+for i in range(100):
+    for j in range(100):
+        if g.has_edge(i,j):
+            tmp=float(min(q[i],q[j]))
+            if tmp<th[i,j]:
+                continue
+            q[i]-=tmp
+            q[j]-=tmp
+            print tmp*abs(p[i]-p[j]) 
+            sw+=tmp*abs(p[i]-p[j])
+print sw
+
